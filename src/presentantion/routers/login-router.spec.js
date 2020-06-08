e1917b9bcda6939b2fc2c55cb8b1322325a3f803
lucasbezerra <lucasbezerra@serel.com.br>
2020-06-08 07:50:23 -0300

    First Test

---
+++ src/presentantion/routers/login-router.spec.js
@@ -0,0 +1,37 @@
+class LoginRouter {
+  route(httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400,
+      };
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('should return 400 if no email is provided', () => {
+    const sut = new LoginRouter();
+
+    const httpRequest = {
+      body: 'any_password',
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test('should return 400 if no password is provided', () => {
+    const sut = new LoginRouter();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
